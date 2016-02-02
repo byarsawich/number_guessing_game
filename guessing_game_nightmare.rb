b64033514c6008d2e0c771be455a5c49c861e0f4
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-02 16:44:58 -0500

    Added logic for changing size of guess range and reaction if computer wins or loses.

--- guessing_game_nightmare.rb
+++ guessing_game_nightmare.rb
@@ -22,6 +22,9 @@ def get_user_response
   return gets.chomp.to_i
 end
 
+LOWER = 0
+CORRECT = 1
+HIGHER = 2
 count_guesses = 0
 puts "Would you like to play a game?\nPick a range of numbers and the number of guesses I can use and I will try guess your number."
 lower_limit = get_lower_limit
@@ -38,6 +41,12 @@ end
 begin
   my_range = (lower_limit..upper_limit).to_a
   my_guess = lower_limit + (upper_limit - lower_limit) / 2
+  count_guesses += 1
+  #if the array my_range has no elements then the user is lying about their number
+  if my_range.length == 0
+    puts "Something is off here.  You wouldn't be cheating to win would you?"
+    exit
+  end
   #check if only 1 number left in the array.  if there is then the computer has won
   if my_range.length == 1
     puts "Your number is #{my_range[0]} and it only took me #{count_guesses} guesses!"
@@ -45,4 +54,17 @@ begin
   end
   puts "My guess is #{my_guess}."
   user_input = get_user_response
-end
+  if user_input == CORRECT
+    puts "Your number was #{my_guess} and it only took me #{count_guesses} guesses!"
+    exit
+  elsif user_input == LOWER
+    #answer is lower then guess so change our upper limit to the guess - 1 so it
+    #is not included in anymore guesses
+    upper_limit = my_guess - 1
+  else
+    #answer is higher then guess so change our lower limit to the guess + 1 so it
+    #is not included in anymore guesses
+    lower_limit = my_guess + 1
+  end
+end while count_guesses < num_guesses
+puts "I have failed.  You win"

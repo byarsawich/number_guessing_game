efda637a8ffb8b1d76fbc8debc48d8aa931ae0e9
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-02 13:51:55 -0500

    Added checks for total number of guesses and going over 5 total guesses.

--- guessing_game.rb
+++ guessing_game.rb
@@ -20,11 +20,22 @@ UPPERLIMIT = 100
 num_guesses = 0
 random_number = get_rand(UPPERLIMIT)
 legal_number = false
+guesses = Array.new
 begin
   begin
     guess = get_input
     legal_number = is_integer?
   end while !legal_number
+  num_guesses += 1
   if check_guess?(random_number, guess)
+    puts "Congrats on guessing that the number was #{guess}"
+    exit
+  elsif num_guesses >=5
+    puts "The number was #{random_number} and you kinda suck at this"
+    exit
+  elsif guesses.include?(guess)
+    puts "Because your guess wasn't already wrong the first time you guessed it"
+  elsif 
+
   end
 end while
